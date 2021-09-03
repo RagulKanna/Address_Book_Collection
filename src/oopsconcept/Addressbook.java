bb6cba42fae3017d654dd5655a99083a992594bb
RagulKanna <ragulragi619@gmail.com>
2021-09-03 22:47:53 +0530

    Added:Hai

--- src/oopsconcept/Addressbook.java
+++ src/oopsconcept/Addressbook.java
@@ -45,27 +45,37 @@ class Addressbookoptions
             entries++;
     }
 
-    public void display(int items)
-    {
+    public void display(int items) {
         System.out.println("\n\nDisplaying  the contact details....");
-        for(int i=0;i<items;i++)
-        {
-            int j=i+1;
-            System.out.println("\n\nThe entry "+j+" is Contact detail of "+ContactdetailsArray[i].firstname);
-            System.out.print("\nFirst name: "+ContactdetailsArray[i].firstname);
-            System.out.print("\nLast name: "+ContactdetailsArray[i].lastname);
-            System.out.print("\nAddress: "+ContactdetailsArray[i].address);
-            System.out.print("\nCity: "+ContactdetailsArray[i].city);
-            System.out.print("\nState: "+ContactdetailsArray[i].state);
-            System.out.print("\nzip: "+ContactdetailsArray[i].zip);
-            System.out.print("\nPhone number: "+ContactdetailsArray[i].phonenumber);
-            System.out.print("\nEmailId: "+ContactdetailsArray[i].email);
+        for (int i = 0; i < items; i++) {
+            int j = i + 1;
+            System.out.println("\n\nThe entry " + j + " is Contact detail of " + ContactdetailsArray[i].firstname);
+            System.out.print("\nFirst name: " + ContactdetailsArray[i].firstname);
+            System.out.print("\nLast name: " + ContactdetailsArray[i].lastname);
+            System.out.print("\nAddress: " + ContactdetailsArray[i].address);
+            System.out.print("\nCity: " + ContactdetailsArray[i].city);
+            System.out.print("\nState: " + ContactdetailsArray[i].state);
+            System.out.print("\nzip: " + ContactdetailsArray[i].zip);
+            System.out.print("\nPhone number: " + ContactdetailsArray[i].phonenumber);
+            System.out.print("\nEmailId: " + ContactdetailsArray[i].email);
 
         }
     }
 
+        public void edit(int item,String first, String last, String address, String city, String state, int zip, long phonenumber, String email)
+        {
+            ContactdetailsArray[item]=new Contactdetails(first,last,address,city,state,zip,phonenumber,email);
+        }
 
+    public void delete(int e)
+    {
+        for(int i=e;i<entries-1;i++) {
+            int j=i+1;
+            ContactdetailsArray[i] = ContactdetailsArray[j];
+        }
+    }
 }
+
 public class Addressbook {
 
     public static void main(String[] args) {
@@ -80,6 +90,8 @@ public class Addressbook {
             System.out.println("\n\n Here are some option for addressbook");
             System.out.println("1.Add the contact...");
             System.out.println("2.Display the contact....");
+            System.out.println("3.Edit the contact...");
+            System.out.println("4.Delete the contact...");
 
 
 
@@ -134,7 +146,85 @@ public class Addressbook {
                 }
                 break;
 
+                case 3:
+                {
+                    if(members == 0)
+                        System.out.println("\n There is no contact to edit!!!!");
+                    else {
+                        option.display(members);
 
+                        int e = 0;
+                        int flag=0;
+
+                        for (int j = 0; j < members; j++) {
+                            System.out.print("\n\nWhich person's detail you want edit: ");
+                            String name = s.next();
+                            if (name.equals(namelist[j])){
+                                e = j;
+                                j = members + 1;
+                                flag=1;
+                                String first, last, address, email, city, state;
+                                int zip;
+                                long phonenumber;
+                                System.out.println("\n\n Enter the details to edit :");
+                                System.out.print("\nEnter the First name: ");
+                                first = s.next();
+                                namelist[e] = first;
+                                System.out.print("\nEnter the Last name: ");
+                                last = s.next();
+                                System.out.print("\nEnter the Address: ");
+                                address = s.next();
+                                System.out.print("\nEnter the city: ");
+                                city = s.next();
+                                System.out.print("\nEnter the state: ");
+                                state = s.next();
+                                System.out.print("\nEnter the zip: ");
+                                zip = s.nextInt();
+                                System.out.print("\nEnter the phone number: ");
+                                phonenumber = s.nextLong();
+                                System.out.print("\nEnter the EmailID: ");
+                                email = s.next();
+                                option.edit(e, first, last, address, city, state, zip, phonenumber, email);
+                            }
+                        }
+                        if(flag == 0)
+                            System.out.println("\n enter the correct name!!!!");
+                    }
+                }
+                break;
+
+                case 4: {
+                    if (members == 0)
+                        System.out.println("\n There is no contact to delete!!!!");
+                    else {
+                        option.display(members);
+
+
+                        int e;
+                        int flag = 0;
+
+                        for (int j = 0; j < members; j++) {
+                            System.out.print("\n\nWhich person's detail you want delete: ");
+                            String name = s.next();
+                            if (name.equals(namelist[j])) {
+                                e = j;
+                                option.delete(e);
+                                j = members + 1;
+                                flag = 1;
+                                for (int k = e; k < members - 1; k++) {
+                                    namelist[k] = namelist[k + 1];
+                                }
+                            }
+
+                        }
+
+                        if(flag==1)
+                        members--;
+                        else
+                            System.out.println("\n enter correct name!!!!");
+                    }
+                }
+                break;
 
             }
             System.out.print("\n\nDo you want to continue press 'y' else 'n':" );
